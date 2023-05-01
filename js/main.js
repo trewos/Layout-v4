b7a7b67e84ebdae4cd6f4250ddc5cf99bb4cdcdd
trewos <n.samokhvalov@edu.internetdevels.com>
2023-05-01 18:12:30 +0300

    "Added registration form, information and slider + adaptive"

--- js/main.js
+++ js/main.js
@@ -3,3 +3,30 @@ $(document).ready(function() {
         $('.header__menu, .header__burger').toggleClass('active');
     });
 });
+$(document).ready(function(){
+    $('.slider__item').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
+});
